89f5fddb2b9bafc2eb32e69b951b641d98311df0
jaffasunny <jaffer.sunny125@gmail.com>
2022-04-11 21:51:41 +0500

    Backend completed

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,6 +7,8 @@ const {
 	deleteGoal,
 } = require("../controllers/goalController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 // router.get("/", getGoals);
 
 // router.post("/", setGoal);
@@ -16,7 +18,7 @@ const {
 // router.delete("/:id", deleteGoal);
 
 // Short hand way to write above stuff
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
